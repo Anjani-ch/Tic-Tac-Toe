9529a0c7abce74ac86261444fa1a19c27da31c14
Anjani <pranav.ch.12@outlook.com>
2022-02-21 08:04:42 +0100

    Updated function syntax

--- js/index.js
+++ js/index.js
@@ -9,15 +9,9 @@ let currentPlayer;
 let isPlaying;
 let board;
 
-window.addEventListener('DOMContentLoaded', initNewGame);
-boardElement.addEventListener('click', handleBoardClick);
-restartBtn.addEventListener('click', initNewGame);
+const changeOutput = text => outputElement.innerText = text;
 
-function changeOutput(text) {
-    outputElement.innerText = text;
-}
-
-function initNewGame() {
+const initNewGame = _ => {
     const spots = Array.from(document.querySelectorAll('.spot'));
 
     currentPlayer = PLAYER_1;
@@ -40,7 +34,53 @@ function initNewGame() {
     restartBtn.classList.add('hidden');
 }
 
-function handleBoardClick(e) {
+const makePlayerMove = spot=> {
+    spot.classList.remove('empty');
+    spot.classList.add(currentPlayer);
+}
+
+const makeAiMove = _ => {
+    const spots = Array.from(document.querySelectorAll('.spot'));
+
+    for(let i = 0; i < 100; i++) {
+        const randomIndex = Math.round(Math.random() * (spots.length - 1));
+
+        const spot = spots[randomIndex];
+        const isEmpty = spot.classList.contains('empty');
+
+        if(isEmpty) {
+            spot.classList.remove('empty');
+            spot.classList.add(currentPlayer);
+            break;
+        }
+    }
+}
+
+const swapTurns = _ => {
+    const isPlayer1 = currentPlayer === PLAYER_1;
+    const isPlayer2 = currentPlayer === PLAYER_2;
+
+    if(isPlayer1) currentPlayer = PLAYER_2;
+    if(isPlayer2) currentPlayer = PLAYER_1;
+}
+
+
+const mapBoard = _ => {
+    const spots = Array.from(document.querySelectorAll('.spot'));
+
+    spots.forEach(spot => {
+        const rowIndex = parseInt(spot.getAttribute('data-row'), 10);
+        const colIndex = parseInt(spot.getAttribute('data-col'), 10);
+
+        const isPlayer1Value = spot.classList.contains(PLAYER_1);
+        const isPlayer2Value = spot.classList.contains(PLAYER_2);
+
+        if(isPlayer1Value) board[rowIndex][colIndex] = PLAYER_1;
+        if(isPlayer2Value) board[rowIndex][colIndex] = PLAYER_2;
+    });
+}
+
+const handleBoardClick = e => {
     const spot = e.target;
     const isSpot = spot.classList.contains('spot');
 
@@ -64,77 +104,27 @@ function handleBoardClick(e) {
         }
     }
 }
-function makePlayerMove(spot) {
-    spot.classList.remove('empty');
-    spot.classList.add(currentPlayer);
+
+const isWinConditionMet = (spot1, spot2, spot3) => spot1 == spot2 && spot2 == spot3 && spot1 !== '';
+
+const isHorizontalWin = _ => {
+    for (let i = 0; i < 3; i++) if (isWinConditionMet(board[i][0], board[i][1], board[i][2])) return true;
 }
 
-function makeAiMove() {
-    const spots = Array.from(document.querySelectorAll('.spot'));
-
-    for(let i = 0; i < 100; i++) {
-        const randomIndex = Math.round(Math.random() * (spots.length - 1));
-
-        const spot = spots[randomIndex];
-        const isEmpty = spot.classList.contains('empty');
-
-        if(isEmpty) {
-            spot.classList.remove('empty');
-            spot.classList.add(currentPlayer);
-            break;
-        }
-    }
+const isVerticalWin = _ => {
+    for (let i = 0; i < 3; i++) if (isWinConditionMet(board[0][i], board[1][i], board[2][i])) return true;
 }
 
-function swapTurns() {
-    const isPlayer1 = currentPlayer === PLAYER_1;
-    const isPlayer2 = currentPlayer === PLAYER_2;
-
-    if(isPlayer1) currentPlayer = PLAYER_2;
-    if(isPlayer2) currentPlayer = PLAYER_1;
-}
-
-
-function mapBoard() {
-    const spots = Array.from(document.querySelectorAll('.spot'));
-
-    spots.forEach(spot => {
-        const rowIndex = parseInt(spot.getAttribute('data-row'), 10);
-        const colIndex = parseInt(spot.getAttribute('data-col'), 10);
-
-        const isPlayer1Value = spot.classList.contains(PLAYER_1);
-        const isPlayer2Value = spot.classList.contains(PLAYER_2);
-
-        if(isPlayer1Value) board[rowIndex][colIndex] = PLAYER_1;
-        if(isPlayer2Value) board[rowIndex][colIndex] = PLAYER_2;
-    });
-}
-
-function isWinConditionMet(spot1, spot2, spot3) {
-    return spot1 == spot2 && spot2 == spot3 && spot1 !== '';
-}
-
-function isHorizontalWin() {
-    for (let i = 0; i < 3; i++) {
-        if (isWinConditionMet(board[i][0], board[i][1], board[i][2])) return true;
-    }
-}
-
-function isVerticalWin() {
-    for (let i = 0; i < 3; i++) {
-        if (isWinConditionMet(board[0][i], board[1][i], board[2][i])) return true;
-    }
-}
-
-function isDiagonalWin() {
+const isDiagonalWin = _ => {
     let result = false;
+
     if (isWinConditionMet(board[0][0], board[1][1], board[2][2])) result = true;
     if (isWinConditionMet(board[2][0], board[1][1], board[0][2])) result = true;
 
     return result;
 }
 
-function checkWin() {
+const checkWin = _ => {
     const spots = Array.from(document.querySelectorAll('.spot'));
 
     const isDraw = spots.every(spot => !spot.classList.contains('empty')); // Check if no spots are empty
@@ -153,3 +143,7 @@ function checkWin() {
         restartBtn.classList.remove('hidden');
     }
 }
+
+window.addEventListener('DOMContentLoaded', initNewGame);
+boardElement.addEventListener('click', handleBoardClick);
+restartBtn.addEventListener('click', initNewGame);
