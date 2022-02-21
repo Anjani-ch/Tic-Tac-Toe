1eba80349745ec99f317a383ce4cbd753766426a
Anjani <pranav.ch.12@outlook.com>
2022-02-21 15:23:49 +0100

    Initial Commit

--- js/index.js
+++ js/index.js
@@ -2,148 +2,103 @@ const boardElement = document.querySelector('#board');
 const outputElement = document.querySelector('#output');
 const restartBtn = document.querySelector('#restart-btn');
 
+const ctx = boardElement.getContext('2d');
+
 const PLAYER_1 = 'x';
 const PLAYER_2 = 'o';
 
-let currentPlayer;
-let isPlaying;
-let board;
+const BOARD_COLOR = 'white';
+const LINE_WIDTH = 4;
 
-const changeOutput = text => outputElement.innerText = text;
+const human = PLAYER_1;
+const ai = PLAYER_2;
+
+const boardWidth = boardElement.width;
+const boardHeight = boardElement.height;
+
+const boardWidthSpaced = boardWidth / 3;
+const boardHeightSpaced = boardHeight / 3;
+
+let startingPlayer = human;
+
+let currentPlayer;
+let board = [
+    [human, ai, human],
+    [ai, human, ai],
+    [human, ai, human]
+];
 
 const initNewGame = _ => {
-    const spots = Array.from(document.querySelectorAll('.spot'));
-
-    currentPlayer = PLAYER_1;
-    isPlaying = true;
+    currentPlayer = startingPlayer;
 
     board = [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
+        [human, ai, human],
+        [ai, human, ai],
+        [human, ai, human]
     ];
-
-    // Reset spots
-    spots.forEach(spot => {
-        spot.classList.add('empty');
-        spot.classList.remove(PLAYER_1);
-        spot.classList.remove(PLAYER_2);
-    });
 
     outputElement.classList.add('hidden');
     restartBtn.classList.add('hidden');
 }
 
-const makePlayerMove = spot => {
-    spot.classList.remove('empty');
-    spot.classList.add(currentPlayer);
-}
+const drawBoard = (board, ctx) => {
+    // Horizontal Lines
+    for(let i = 1; i < board.length; i++) {
+        ctx.beginPath(); 
+        ctx.moveTo(0, boardHeightSpaced * i);
+        ctx.lineTo(boardWidth, boardHeightSpaced * i);
+        ctx.stroke();
+    }
 
-const makeAiMove = _ => {
-    const spots = Array.from(document.querySelectorAll('.spot'));
-
-    for(let i = 0; i < 100; i++) {
-        const randomIndex = Math.round(Math.random() * (spots.length - 1));
-
-        const spot = spots[randomIndex];
-        const isEmpty = spot.classList.contains('empty');
-
-        if(isEmpty) {
-            spot.classList.remove('empty');
-            spot.classList.add(currentPlayer);
-            break;
-        }
+    // Horizontal Lines
+    for(let i = 1; i < board.length; i++) {
+        ctx.beginPath(); 
+        ctx.moveTo(boardWidthSpaced * i, 0);
+        ctx.lineTo(boardWidthSpaced * i, boardHeight);
+        ctx.stroke();
     }
 }
 
-const swapTurns = _ => {
-    const isPlayer1 = currentPlayer === PLAYER_1;
-    const isPlayer2 = currentPlayer === PLAYER_2;
+const drawBoardValues = (board, ctx) => {
+    board.forEach((row, rowIndex) => {
+        row.forEach((cell, cellIndex) => {
+            const x = boardWidthSpaced * cellIndex + boardWidthSpaced / 2;
+            const y = boardHeightSpaced * rowIndex + boardHeightSpaced / 2;
 
-    if(isPlayer1) currentPlayer = PLAYER_2;
-    if(isPlayer2) currentPlayer = PLAYER_1;
-}
+            if(cell === PLAYER_1) {
+                
+            } else if(cell === PLAYER_2) {
+                const radius = boardWidthSpaced / 3;
+                const startAngle = 0;
+                const endAngle = 2 * Math.PI;
 
-
-const mapBoard = _ => {
-    const spots = Array.from(document.querySelectorAll('.spot'));
-
-    spots.forEach(spot => {
-        const rowIndex = parseInt(spot.getAttribute('data-row'), 10);
-        const colIndex = parseInt(spot.getAttribute('data-col'), 10);
-
-        const isPlayer1Value = spot.classList.contains(PLAYER_1);
-        const isPlayer2Value = spot.classList.contains(PLAYER_2);
-
-        if(isPlayer1Value) board[rowIndex][colIndex] = PLAYER_1;
-        if(isPlayer2Value) board[rowIndex][colIndex] = PLAYER_2;
+                ctx.beginPath();
+                ctx.arc(x, y, radius, startAngle, endAngle);
+                ctx.stroke();
+            }
+        });
     });
 }
 
+const update = (board, ctx) => {
+    ctx.clearRect(0, 0, boardWidth, boardHeight);
+
+    ctx.fillStyle = BOARD_COLOR;
+    ctx.lineWidth = LINE_WIDTH;
+
+    drawBoard(board, ctx);
+    drawBoardValues(board, ctx);
+
+    requestAnimationFrame(() => update(board, ctx))
+}
+
 const handleBoardClick = e => {
-    const spot = e.target;
-    const isSpot = spot.classList.contains('spot');
 
-    if(isSpot) {
-        const isEmpty = spot.classList.contains('empty');
-
-        if(isEmpty) {
-            if(isPlaying) {
-                makePlayerMove(spot);
-                mapBoard();
-                checkWin();
-                swapTurns();
-            }
-
-            if(isPlaying) {
-                makeAiMove();
-                mapBoard();
-                checkWin();
-                swapTurns();
-            }
-        }
-    }
 }
 
-const isWinConditionMet = (spot1, spot2, spot3) => spot1 == spot2 && spot2 == spot3 && spot1 !== '';
-
-const isHorizontalWin = _ => {
-    for (let i = 0; i < 3; i++) if (isWinConditionMet(board[i][0], board[i][1], board[i][2])) return true;
-}
-
-const isVerticalWin = _ => {
-    for (let i = 0; i < 3; i++) if (isWinConditionMet(board[0][i], board[1][i], board[2][i])) return true;
-}
-
-const isDiagonalWin = _ => {
-    let result = false;
-
-    if (isWinConditionMet(board[0][0], board[1][1], board[2][2])) result = true;
-    if (isWinConditionMet(board[2][0], board[1][1], board[0][2])) result = true;
-
-    return result;
-}
-
-const checkWin = _ => {
-    const spots = Array.from(document.querySelectorAll('.spot'));
-
-    const isDraw = spots.every(spot => !spot.classList.contains('empty')); // Check if no spots are empty
-
-    let isWin = false;
-
-    isWin = isHorizontalWin() || isVerticalWin() || isDiagonalWin();
-
-    if(isWin || isDraw) {
-        isPlaying = false;
-
-        if(isWin) changeOutput(`${currentPlayer.toUpperCase()}' Wins`);
-        else if(isDraw) changeOutput("It's A Draw");
-
-        outputElement.classList.remove('hidden');
-        restartBtn.classList.remove('hidden');
-    }
-}
-
-window.addEventListener('DOMContentLoaded', initNewGame);
+window.addEventListener('DOMContentLoaded', () => {
+    initNewGame();
+    update(board, ctx);
+});
 boardElement.addEventListener('click', handleBoardClick);
 restartBtn.addEventListener('click', initNewGame);
